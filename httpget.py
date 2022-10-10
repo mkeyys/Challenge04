88ec5c67c8046f38eff99688466846771e3ab3a3
mk4y <113781267+mkeyys@users.noreply.github.com>
2022-10-11 00:04:19 +0700

    Update httpget.py

--- httpget.py
+++ httpget.py
@@ -1,10 +1,9 @@
 import socket
+import re
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("blogtest.vnprogramming.com",80))
 s.send(b"GET / HTTP/1.1\r\nHost:blogtest.vnprogramming.com\r\n\r\n")
-r = s.makefile()
-print("Title of Wordpress is: \n")
-for line in r.readlines():
-    if '<title>' in line:
-        print(line[8:18])
-        s.close()
+response = s.recv(4096).decode()
+s.close()
+filter_res = re.search(r">.*&", response).group()
+print(filter_res[1:-1:])
