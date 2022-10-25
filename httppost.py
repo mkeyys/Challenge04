848afceb830a9ab6ef51180b7c4b5a4ba5ef05a5
mk4y <113781267+mkeyys@users.noreply.github.com>
2022-10-25 09:13:19 +0700

    Update httppost.py

--- httppost.py
+++ httppost.py
@@ -1,6 +1,20 @@
 import socket
+import argparse, re
+from urllib.parse import urlparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", dest="target_host", help="url_target")
+parser.add_argument("--username", dest="username", help="user")
+parser.add_argument("--password", dest="password", help="password")
+parser.usage = parser.format_help()
+args = parser.parse_args()
+
+host = args.target_host
+user = args.username
+passwd = args.password
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("blogtest.vnprogramming.com",80))
+s.connect((urlparse(f'{host}').netloc,80))
 
 header = """POST /wp-login.php HTTP/1.1\r
 Host: blogtest.vnprogramming.com\r
@@ -9,8 +23,7 @@ Content-Length: {content_length}\r
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r
 Connection: keep-alive\r
 \r\n"""
-user= input("user:")
-passwd = input("pass:")
+
 body = "log={}&pwd={}\n\n".format(user,passwd)
 header_bytes = header.format(content_length=len(body))
 request = (header_bytes + body).encode()
