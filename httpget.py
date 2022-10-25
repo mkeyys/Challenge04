ac97778c0c4179371bd7920c467143e446531524
mk4y <113781267+mkeyys@users.noreply.github.com>
2022-10-25 09:12:55 +0700

    Update httpget.py

--- httpget.py
+++ httpget.py
@@ -1,7 +1,14 @@
 import socket
-import re
+import argparse, re
+from urllib.parse import urlparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("--url", dest="target_host", help="url target argument")
+args = parser.parse_args()
+
+host = args.target_host
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("blogtest.vnprogramming.com",80))
+s.connect((urlparse(f'{host}').netloc,80))
 s.send(b"GET / HTTP/1.1\r\nHost:blogtest.vnprogramming.com\r\n\r\n")
 response = s.recv(4096).decode()
 s.close()
